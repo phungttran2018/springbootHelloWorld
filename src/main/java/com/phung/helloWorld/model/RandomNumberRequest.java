1e1bbeeab5194905446f5db5073ebdb8c204e80e
phungttran2018 <phungttran2018@gmail.com>
2022-01-20 20:43:06 -0600

    commit all

---
+++ src/main/java/com/phung/helloWorld/model/RandomNumberRequest.java
@@ -0,0 +1,22 @@
+package com.phung.helloWorld.model;
+
+public class RandomNumberRequest {
+    private Integer i;
+    private String country;
+
+    public Integer getI() {
+        return i;
+    }
+
+    public void setI(Integer i) {
+        this.i = i;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
